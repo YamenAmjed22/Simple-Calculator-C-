971e1cc238e66f0ad4dd84bab7d50438edcede3f
Yamen Amjed <yamenamjed2022@gmail.com>
2024-12-24 05:58:53 -0800

    Initial commit

---
+++ CalculatorHW2/Calculator .cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHW2
+{
+    internal class Calculator
+    {
+        // properties
+        private double firstNumber;
+        private double secoundNumber;
+        
+        // Constructor  
+        public Calculator(double x, double y)
+        {
+            this.firstNumber = x;
+            this.secoundNumber = y;
+        }
+        public Calculator() 
+        { 
+            this.firstNumber = 0;
+            this.secoundNumber = 0;
+        }
+        public double Add() 
+        { 
+            return this.firstNumber + this.secoundNumber;
+        }
+        public double Subtract()
+        {
+            return this.firstNumber - this.secoundNumber;
+        }
+        public double Multiply()
+        {
+            return this.firstNumber * this.secoundNumber;
+        }
+        public string Divied()
+        {
+            if (this.secoundNumber == 0)
+            {
+
+                return "We can\'t Divied by 0 ";
+
+            }
+            else 
+            {
+                return (this.firstNumber / this.secoundNumber).ToString();    
+            }
+        }
+        // Getter and Setter for x
+        public double GetFirstNumber()
+        {
+            return this.firstNumber;
+        }
+
+        public void SetFirstNumber(double value)
+        {
+            this.firstNumber = value;
+        }
+
+        // Getter and Setter for y
+        public double GetSecoundNumber()
+        {
+            return this.secoundNumber;
+        }
+
+        public void SetY(double value)
+        {
+            this.secoundNumber = value;
+        }
+
+    }
+}
---
+++ CalculatorHW2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in my calculator");
+            Console.Write("Enter value for First Number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter value for Secound Number: ");
+            double secoundNumber = Convert.ToDouble(Console.ReadLine());
+            Calculator calculator = new Calculator(firstNumber,secoundNumber);
+            Console.WriteLine($"The Sum For ({calculator.GetFirstNumber()} + {calculator.GetSecoundNumber()}) = {calculator.Add()}");
+            Console.WriteLine($"The Subtraction For({calculator.GetFirstNumber()} - {calculator.GetSecoundNumber()}) = {calculator.Subtract()}");
+            Console.WriteLine($"The Multiplaction For ({calculator.GetFirstNumber()} * {calculator.GetSecoundNumber()}) = {calculator.Multiply()}");
+            Console.WriteLine($"The Diviation For ({calculator.GetFirstNumber()} / {calculator.GetSecoundNumber()}) = {calculator.Divied()}");
+
+        }
+    }
+}
